44f33dd0075fb9b2529cda574cf24cf24534359e
Sebastian Salazar <sebastian.salazar@ariadnacg.com>
2019-07-12 17:40:19 -0500

    Item 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles/App.scss';
+import Item from "./components/Item";
+
+class App extends Component {
+  render() {
+    return (
+      <Item />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Item = () => {
+    return (
+        <div className="Item">
+            <h1>Item 1</h1>
+        </div>
+    );
+};
+
+export default Item;
